64779d091cdc293e041821dbf68b2f2bb573bf78
harisangar <ganambihai10@gmail.com>
2024-04-15 13:49:30 +0530

    image name change

--- public/images/index.js
+++ public/images/index.js
@@ -1,4 +1,4 @@
-import heroimage from '../images/heroimage.JPG';
+import heroimage from '../images/heroimage.jpg';
 import dev from '../images/dev.jpg';
 import one from '../images/1.jpg';
 import two from '../images/2.jpg';
@@ -6,4 +6,4 @@ import three from '../images/3.jpg';
 import four from '../images/4.jpg';
 import five from '../images/5.jpg';
 import six from '../images/6.jpg';
-export { heroimage, dev,one,three,two,four,five,six };
+export { heroimage, dev, one, three, two, four, five, six };
